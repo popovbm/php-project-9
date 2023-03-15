7b8aded2cf273874946b2e428ebeef0711205541
Ivan Popov <popovbm@gmail.com>
2023-03-15 11:51:57 +0300

    step 2 fix main page route

--- public/index.php
+++ public/index.php
@@ -2,21 +2,19 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->add(MethodOverrideMiddleware::class);
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
-
-$app->get('/hello', function ($request, $response) {
     $renderer = new PhpRenderer(__DIR__ . '/../templates');
-    return $renderer->render($response, "hello.phtml");
+    return $renderer->render($response, "main.phtml");
 });
 
 $app->run();
